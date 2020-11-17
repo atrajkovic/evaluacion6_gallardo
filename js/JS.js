410c9dab93a69cf9229ab654c0cde3732db28c10
Araceli Gallardo <69484046+atrajkovic@users.noreply.github.com>
2020-11-17 03:57:44 +0100

    Add files via upload

---
+++ js/JS.js
@@ -0,0 +1,13 @@
+
+
+function calculo () {
+
+
+
+var numero = parseFloat(document.getElementById("numero").value) ;
+document.getElementById("numeroingresado").innerHTML = numero + "$" ;
+document.getElementById("numeroconiva").innerHTML = numero * 1.21 + "$";
+
+
+}
+
---
+++ js/codjs.js
@@ -0,0 +1,9 @@
+
+
+function calculariva(){
+var numero = parseFloat(document.getElementById("numero").value) ;
+document.getElementById("numeroingresado").innerHTML = numero + "$" ;
+document.getElementById("numeroconiva").innerHTML = numero * 1.21 + "$";
+
+
+}
